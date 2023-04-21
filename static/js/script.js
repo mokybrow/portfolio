bcf67a097a1ceebf83c4a87c45699f75efe76f24
Mikhail Panin <64284425+mokybrow@users.noreply.github.com>
2023-04-22 00:22:29 +0200

    Ааааа

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,8 @@ const menuBtn = document.querySelector('.menu__icon')
 
 const body = document.body;
 
+const mediaQuery = window.matchMedia('(min-width: 1000px)')
+
 if (menu && menuBtn) {
     menuBtn.addEventListener('click', e => {
         menu.classList.toggle('active')
@@ -18,6 +20,7 @@ if (menu && menuBtn) {
             body.classList.remove('lock')
         })
     })
+
     menu.querySelectorAll('.menu__link__btn').forEach(link => {
         link.addEventListener('click', () => {
             menu.classList.remove('active')
@@ -27,19 +30,141 @@ if (menu && menuBtn) {
     })
 }
 
-/*===========================================*/
+function handleTabletChange(e) {
+    if (e.matches) {
+        menu.classList.remove('active')
+        menuBtn.classList.remove('active')
+        body.classList.remove('lock')
+    }
+}
 
-// const anchors = document.querySelectorAll('a[href*="#"]');
-//
-// anchors.forEach(anchor => {
-// 	anchor.addEventListener('click', event => {
-// 		event.preventDefault();
-//
-// 		const blockID = anchor.getAttribute('href').substring(1);
-//
-// 		document.getElementById(blockID).scrollIntoView({
-// 			behavior: 'smooth',
-// 			block: 'start'
-// 		})
-// 	})
-// })
+mediaQuery.addListener(handleTabletChange)
+handleTabletChange(mediaQuery)
+
+
+$(document).ready(function () {
+    $(".center").slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '20vw',
+        draggable: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        easing: 'easeInOutSine',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    arrows: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    centerPadding: '15vw',
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    centerPadding: '10vw',
+                }
+            }
+        ]
+    });
+});
+
+//Анимация текста
+
+
+//плавное скрытие хидера
+$(document).ready(() => { // DOM готов к взаимодейтсвию
+
+	const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
+
+		const header = $('.header') // находим header и записываем в константу
+
+		let prevScroll = $(window).scrollTop() // узнаем на сколько была прокручена страница ранее
+		let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
+
+		$(window).scroll(() => { // при прокрутке страницы
+
+			currentScroll = $(window).scrollTop() // узнаем на сколько прокрутили страницу
+
+			const headerHidden = () => header.hasClass('header_hidden') // узнаем скрыт header или нет
+
+			if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
+				header.addClass('header_hidden') // то скрываем header
+			}
+			if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
+				header.removeClass('header_hidden') // то отображаем header
+			}
+
+			prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
+
+		})
+
+	}
+
+	onScrollHeader() // вызываем основную функцию onScrollHeader
+
+})
+
+
+//Бегущая строка
+
+var $tickerWrapper = $(".tickerwrapper");
+var $list = $tickerWrapper.find("ul.list");
+var $clonedList = $list.clone();
+var listWidth = 10;
+
+$list.find("li").each(function (i) {
+			listWidth += $(this, i).outerWidth(true);
+});
+
+var endPos = $tickerWrapper.width() - listWidth;
+
+$list.add($clonedList).css({
+	"width" : listWidth + "px"
+});
+
+$clonedList.addClass("cloned").appendTo($tickerWrapper);
+
+
+//TimelineMax
+var infinite = new TimelineMax({repeat: -1, paused: true});
+var time = 35;
+
+infinite
+  .fromTo($list, time, {rotation:0.01,x:0}, {force3D:true, x: -listWidth, ease: Linear.easeNone}, 0)
+  .fromTo($clonedList, time, {rotation:0.01, x:listWidth}, {force3D:true, x:0, ease: Linear.easeNone}, 0)
+  .set($list, {force3D:true, rotation:0.01, x: listWidth})
+  .to($clonedList, time, {force3D:true, rotation:0.01, x: -listWidth, ease: Linear.easeNone}, time)
+  .to($list, time, {force3D:true, rotation:0.01, x: 0, ease: Linear.easeNone}, time)
+  .progress(1).progress(0)
+  .play();
+
+//Pause/Play
+// $tickerWrapper.on("mouseenter", function(){
+// 	infinite.pause();
+// }).on("mouseleave", function(){
+// 	infinite.play();
+// });
+
+
+//Бегущая строка
