d825e22e76627e8c3136bdc96a8e1b6f76794f95
Mikhail Panin <64284425+mokybrow@users.noreply.github.com>
2023-04-23 23:26:35 +0200

    Добавлен загрузочный экран

--- static/js/script.js
+++ static/js/script.js
@@ -95,33 +95,59 @@ $(document).ready(function () {
 //плавное скрытие хидера
 $(document).ready(() => { // DOM готов к взаимодейтсвию
 
-	const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
+    const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
 
-		const header = $('.header') // находим header и записываем в константу
+        const header = $('.header') // находим header и записываем в константу
 
-		let prevScroll = $(window).scrollTop() // узнаем на сколько была прокручена страница ранее
-		let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
+        let prevScroll = $(window).scrollTop() // узнаем на сколько была прокручена страница ранее
+        let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
 
-		$(window).scroll(() => { // при прокрутке страницы
+        $(window).scroll(() => { // при прокрутке страницы
 
-			currentScroll = $(window).scrollTop() // узнаем на сколько прокрутили страницу
+            currentScroll = $(window).scrollTop() // узнаем на сколько прокрутили страницу
 
-			const headerHidden = () => header.hasClass('header_hidden') // узнаем скрыт header или нет
+            const headerHidden = () => header.hasClass('header_hidden') // узнаем скрыт header или нет
 
-			if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
-				header.addClass('header_hidden') // то скрываем header
-			}
-			if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
-				header.removeClass('header_hidden') // то отображаем header
-			}
+            if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
+                header.addClass('header_hidden') // то скрываем header
+            }
+            if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
+                header.removeClass('header_hidden') // то отображаем header
+            }
 
-			prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
+            prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
 
-		})
+        })
 
-	}
+    }
 
-	onScrollHeader() // вызываем основную функцию onScrollHeader
+    onScrollHeader() // вызываем основную функцию onScrollHeader
 
 })
 
+//Анимация интро
+
+let intro = document.querySelector('.intro')
+let logo = document.querySelector('.logo-header')
+let logoSpan = document.querySelectorAll('.logo')
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active')
+            }, (idx + 1) * 400);
+        });
+
+        setTimeout(() => {
+            logoSpan.forEach((span, idx) => {
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            })
+        }, 2000);
+        setTimeout(() => {
+            intro.style.top = '-100vh'
+        }, 2600);
+    })
+})
