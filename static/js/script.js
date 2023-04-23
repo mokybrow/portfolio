7abcc33ee74b735cf5822dd4373fe00ad83f4b13
Mikhail Panin <64284425+mokybrow@users.noreply.github.com>
2023-04-24 01:05:15 +0200

    Добавлен шум на фон

--- static/js/script.js
+++ static/js/script.js
@@ -153,3 +153,28 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 })
 
+var StickyElement = function(node){
+    var doc = $(document),
+    fixed = false,
+    anchor = node.find('.sticky-anchor'),
+    content = node.find('.sticky-content');
+    var onScroll = function(e){
+        var docTop = doc.scrollTop(),
+        anchorTop = anchor.offset().top;
+        if(docTop > anchorTop){
+            if(!fixed){
+                anchor.height(content.outerHeight());
+                content.addClass('fixed');
+                fixed = true;
+            }
+            } else {
+            if(fixed){
+                anchor.height(0);
+                content.removeClass('fixed');
+                fixed = false;
+            }
+        }
+    };
+    $(window).on('scroll', onScroll);
+};
+var sticky = new StickyElement($('.sticky-element'));
